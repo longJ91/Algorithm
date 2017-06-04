0deff92b199a89c1f4a2aa268100b30cded3ec68
JiJeongHan <wlwjdgks123@gmail.com>
2017-06-04 23:52:54 +0900

    Update Dynamic Programming

---
+++ Baekjoon/Dynamin Programming/타일링2.cpp
@@ -0,0 +1,17 @@
+// 문제 : https://www.acmicpc.net/problem/11727
+// 규칙을 활용
+#include <iostream>
+using namespace std;
+long long R[1001];
+int main() {
+	int N;
+	cin >> N;
+	R[1] = 1;
+	R[2] = 3;
+	for (int i = 3; i <= N; i++) {
+		R[i] = R[i - 1] + 2*R[i - 2];
+		R[i] = R[i] % 10007;
+	}
+	cout << R[N] << endl;
+	return 0;
+}
