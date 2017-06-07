532d793bda45e2815dc73edfe4c1c614ff9545c2
JiJeongHan <wlwjdgks123@gmail.com>
2017-06-07 18:59:26 +0900

    Update Dynamic Programming

---
+++ Baekjoon/Dynamin Programming/가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,19 @@
+// ¹®Á¦ : https://www.acmicpc.net/problem/11722
+// Botton Up
+#include <iostream>
+using namespace std;
+int A[1001];
+long long D[1001];
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) cin >> A[i];
+	for (int i = 1; i <= N; i++) {
+		D[i] = 1;
+		for (int j = 2; j <= i; j++) if (A[i] < A[j] && D[i] < D[j] + 1) D[i] = D[j] + 1;
+	}
+	long long max = 0;
+	for (int i = 1; i <= N; i++) if (max < D[i]) max = D[i];
+	cout << max << endl;
+	return 0;
+}
