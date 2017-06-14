5e5317f6209c5c594182a51af3686016dc469650
JiJeongHan <wlwjdgks123@gmail.com>
2017-06-14 16:26:12 +0900

    Update Dynamic Programming

---
+++ Baekjoon/Dynamin Programming/피보나치 수 2.cpp
@@ -0,0 +1,13 @@
+// ¹®Á¦ : https://www.acmicpc.net/submit/2748/6013011
+#include <iostream>
+using namespace std;
+long long fib[50];
+int main() {
+	int n;
+	cin >> n;
+	fib[0] = 0;
+	fib[1] = 1;
+	for (int i = 2; i <= n; i++) fib[i] = fib[i - 1] + fib[i - 2];
+	cout << fib[n] << endl;
+	return 0;
+}
