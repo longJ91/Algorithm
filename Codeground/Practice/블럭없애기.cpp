9eefeec6f67cf8fb94c960e289da76cdcff18a89
JiJeongHan <wlwjdgks123@gmail.com>
2017-06-18 00:46:15 +0900

    Update Practice of Codeground

--- Codeground/Practice/블럭없애기.cpp
+++ Codeground/Practice/블럭없애기.cpp
@@ -4,7 +4,8 @@
 #include <stdio.h>
 
 int N[100002];
-int n, temp;
+int n;
+unsigned long long temp;
 
 int main(void) {
 	/* 아래 freopen 함수는 input.txt를 read only 형식으로 열고, 표준입력(키보드) 대신 input.txt 로 부터 읽어오겠다는 의미의 코드입니다.
@@ -12,7 +13,7 @@ int main(void) {
 	그 아래에서 scanf 함수를 사용하여 표준입력 대신 input.txt 파일로 부터 입력값을 읽어 올 수 있습니다.
 	또한, 본인 PC에서 freopen 함수를 사용하지 않고 표준입력을 사용하여 테스트하셔도 무방합니다.
 	단, Codeground 시스템에서 "제출하기" 할 때에는 반드시 freopen 함수를 지우거나 주석(//) 처리 하셔야만 합니다. */
-	freopen("input.txt", "r", stdin);
+	//freopen("input.txt", "r", stdin);
 
 	setbuf(stdout, NULL);
 
@@ -29,38 +30,11 @@ int main(void) {
 		for (int i = 1; i <= n; i++) scanf("%d", &N[i]);
 		
 
-		N[0] = N[n + 1] = r = temp = 0;
-
-		int a, b;
-		int A, B, C;
-
-		while (1) {
-			pass = 1;
-			for (int i = 1; i <= n; i++) {
-				if (i == 1) {
-					A = N[0];
-					B = N[1];
-					C = N[2];
-				}
-				else {
-					A = B;
-					B = C;
-					C = N[i+1];
-				}
-				if (B != 0) {
-					N[i]--;
-
-					a = A < C ? A : C;
-
-					N[i] = N[i] > a ? a : N[i];
-					
-					pass = 0;
-				}
-				temp += B;
-			}
-			if (pass == 1 || temp == 0 ) break;
-			r++;
-			temp = 0;
+		N[0] = N[n + 1] = temp = r = 0;
+		for (int i = 1; i <= n; i++) N[i - 1] + 1 > N[i] ? 0 : N[i] = N[i - 1] + 1;
+		for (int i = n; i >= 1; i--) {
+			N[i + 1] + 1 > N[i] ? 0 : N[i] = N[i + 1] + 1;
+			if (r < N[i]) r = N[i];
 		}
 		// 이 부분에서 정답을 출력하십시오.
 		printf("Case #%d\n%d\n", test_case,r);
